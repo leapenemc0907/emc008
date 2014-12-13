62491873f2c93b333e49144393fb5dc9f3d7cf62
chuanlian <yangcl97@gmail.com>
2014-12-13 19:11:28 +0800

    test project

---
+++ src/crm/baidu/com/App.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (C) 2014 Baidu, Inc. All Rights Reserved.
+ */
+package crm.baidu.com;
+
+/**
+ * @author Administrator
+ * 
+ */
+public class App {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("ok");
+    }
+
+}
